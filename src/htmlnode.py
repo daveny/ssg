07fd082388eb8856bf7a71ea783bcf99a45884a3
daveny <davenyen@gmail.com>
2025-02-16 09:04:02 +0100

    fix

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,14 +21,14 @@ class HTMLNode:
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props):
-        super().__init__(tag, value, props)
+        super().__init__(tag=tag, value=value, children=None, props=props)
     
     def to_html(self):
         if self.value == None:
             raise ValueError("All leaf nodes must have a value")
         if self.tag == None:
             return self.value
-        return f"<{self.tag}>{self.value}<\\{self.tag}>"
+        return f"<{self.tag}>{self.value}</{self.tag}>"
     
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
