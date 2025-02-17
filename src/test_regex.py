0c01367822f5e90abbf430b103a676934575a5a0
daveny <davenyen@gmail.com>
2025-02-17 17:20:08 +0100

    images and links

---
+++ src/test_regex.py
@@ -0,0 +1,28 @@
+import unittest
+from helpers import extract_markdown_images, extract_markdown_links
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_regex(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual(extract_markdown_images(text), [('rick roll', 'https://i.imgur.com/aKaOqIh.gif'), ('obi wan', 'https://i.imgur.com/fJRm4Vk.jpeg')])
+    
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://blog.boot.dev"),
+            ],
+            matches,
+        )    
+
+if __name__ == "__main__":
+    unittest.main()
